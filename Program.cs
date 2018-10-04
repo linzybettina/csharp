2aabb1fb928a375b0487ad42e08f0d9badbf874e
linzybettina <43557782+linzybettina@users.noreply.github.com>
2018-10-04 10:27:26 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string signal = "o";
+            while (signal != "x")
+            {
+                Console.WriteLine("enter a signal");
+                signal = Console.ReadLine();
+                Console.WriteLine("received signal:{0}", signal);
+                if (signal == "A")
+                {
+                    Console.WriteLine("fault abort\n\n");
+                    break;
+                   
+                }
+                if (signal == "o")
+                {
+                    Console.WriteLine("all is well");
+                    continue;
+                }
+            }
+        }
+    }
+
+}
